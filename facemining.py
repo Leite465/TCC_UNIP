6271731e94a69a7d26780d5e2c9f31cd4ae7240e
leite465 <gabriel-leite2014@outlook.com>
2020-10-02 17:01:04 -0300

    tried to fix the 'errno 13' problem

--- facemining.py
+++ facemining.py
@@ -9,5 +9,6 @@ dotenv.load_dotenv()
 token = os.environ['USER_ACCESS_TOKEN']
 
 graph = facebook.GraphAPI(access_token=token, version=7.0)
-post = graph.get_object(id='-', fields='posts')
+post = graph.get_object(id='3282934375160574', fields='posts')
 print(post['posts'])
+#Meu Id '3282934375160574'
